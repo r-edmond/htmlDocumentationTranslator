21842ace6d48fd6b4a4df29fe7586d197a972f92
Raynosan <raynosan.edmond@gmail.com>
2021-05-04 18:14:16 -0400

    fixed empty-slideList issue

--- src/main/scala/example/Main.scala
+++ src/main/scala/example/Main.scala
@@ -1,6 +1,7 @@
 package org.nu.htmlDocumentation
 package example
 
+import java.beans.BeanProperty
 import scala.xml.XML
 import java.io.{File, PrintWriter}
 
@@ -10,19 +11,23 @@ object Main extends App {
   /* Step 1: Get Slide Data */
 
   //Variable and Value declarations
-  val okFileExtensions = List("xml, rels")
+  val okFileExtensions = List(".xml", ".rels")
   val dir = "D:\\code\\slides"
   val slideRelDir = "\\slides\\_rels"
   val slideDir = "\\slides"
   val imgDir = "\\media"
 
+  
   //Get List of XMLs
-  val f = getListOfFiles(new File("D:\\code\\slides"), okFileExtensions)
+  val f = getListOfFiles(new File(".\\slides"), okFileExtensions)
+  var e = new File(".\\slides")
 
   //Store Slide Data in List
-  var data = for (n <- f) yield getSlideData(dir + "\\" + n.getName)
+  var data = for (n <- f) yield getSlideData("D:\\code\\slides\\slides\\" + n.getName)
+  println(data.length)
   data = data.sortBy(n => n.slideNum)
 
+  var t1 = getSlideData("D:\\code\\slides\\slides\\slide1.xml")
   //Write slide data to txt, for test
   val pw = new PrintWriter(new File("D:\\code\\slides\\s.txt" ))
 
@@ -30,12 +35,13 @@ object Main extends App {
   data.foreach{
     n => if (n.text.nonEmpty) {
       pw.write("Slide Number: " + n.slideNum + "\n")
+      println(n.text)
       pw.write(n.text)
       pw.write("\n")
     }
   }
-  pw.close()
 
+  println("Slides processed: " + data.length)
   println("Nithyanandam")
 
   //Create Slide Object
@@ -43,9 +49,7 @@ object Main extends App {
 
   //Get List of XML files
   def getListOfFiles(dir: File, extensions: List[String]): List[File] = {
-    dir.listFiles.filter(_.isFile).toList.filter { file =>
-      extensions.exists(file.getName.endsWith(_))
-    }
+    dir.listFiles.filter(_.isFile).toList.filter{file => extensions.exists(file.getName.endsWith(_))}
   }
 
   //Get Slide Data based on Directory
